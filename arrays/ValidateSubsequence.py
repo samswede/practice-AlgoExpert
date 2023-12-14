e7d69aa22bbda8f91fb745ebb837ce34634db4f4
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2023-12-14 09:25:40 +0100

    update

--- arrays/ValidateSubsequence.py
+++ arrays/ValidateSubsequence.py
@@ -1,14 +1,23 @@
 import unittest
 
-def validateSubsequence(array, sequence):
-    return False
+def isValidSubsequence(array, sequence):
+
+    return True
 
 
 class TestProgram(unittest.TestCase):
 
     def test_types(self):
-        self.assertEqual(type(validateSubsequence([1, 2], [1])), bool)
+        self.assertEqual(type(isValidSubsequence([1, 2], [1])), bool)
 
+    def test_base_case(self):
+        self.assertEqual(isValidSubsequence([1, 2], [1]), True)
+        self.assertEqual(isValidSubsequence([1, 2], [2]), True)
+        self.assertEqual(isValidSubsequence([1, 2], [3]), False)
+    
+    """ def test_case_1(self):
+        self.assertEqual(validateSubsequence([1, 2], [1, 2]), True)
+        self.assertEqual(validateSubsequence([1, 2], [2, 1]), False) """
 
 if __name__ == "__main__":
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
