baa174ee5ef9e0fe9719e3761e50463ac20f0723
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2023-12-14 08:24:47 +0100

    init stack

---
+++ stacks/MinMaxStack.py
@@ -0,0 +1,23 @@
+import unittest
+
+# Feel free to add new properties and methods to the class.
+class MinMaxStack:
+    def peek(self):
+        # Write your code here.
+        pass
+
+    def pop(self):
+        # Write your code here.
+        pass
+
+    def push(self, number):
+        # Write your code here.
+        pass
+
+    def getMin(self):
+        # Write your code here.
+        pass
+
+    def getMax(self):
+        # Write your code here.
+        pass
